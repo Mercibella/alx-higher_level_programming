0bd5bb221d4078966786f3ae673115790cc80695
mercibella <mercyachara@gmail.com>
2023-03-30 08:52:04 -0700

    Python - Network #0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes a URL, sends a GET request to the URL, and displays body of the response
-curl -Lsf "$1
+# Script that shows the response body redirecting /route_1
+curl -Ls "$1"
